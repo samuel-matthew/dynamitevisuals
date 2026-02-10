9389405d9809388c5286adff299643243a2319cd
Samuel Matthew <samuelmatthew071@gmail.com>
2026-02-10 13:29:30 +0100

    Update package.json scripts and axios configuration for development mode

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
+// const API = import.meta.env.VITE_API_URL;
+const API = import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/api";
 
 export const api = axios.create({
   baseURL: API,
@@ -9,3 +10,4 @@ export const api = axios.create({
   },
   withCredentials: true, // Enable sending cookies with requests
 });
+ 
